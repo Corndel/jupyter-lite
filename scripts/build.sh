5aa1306366beb2cf85b5343c77a9e032dc4ad279
Simon Haines <nimon@hotmail.co.uk>
2025-04-03 19:48:42 +0100

    more config tweaks

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
-# build the JupyterLite site
 jupyter lite --version
 jupyter lite build --contents content --output-dir dist
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,1 @@
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-export PATH="$ROOT_DIR/bin:$PATH"
-export MAMBA_ROOT_PREFIX="$ROOT_DIR/micromamba"
-
-# Activate micromamba
-eval "$("$ROOT_DIR/bin/micromamba" shell hook -s bash)"
-micromamba activate jupyterenv
-
-# Serve locally
-jupyter lite serve --lite-dir "$ROOT_DIR/dist"
+jupyter lite serve --lite-dir dist
